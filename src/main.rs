589805d43e2ff5de4c5bf2832dfb57bbe32e6552
Bjorn Martens <bjorn@expeditious.nl>
2020-12-18 11:52:12 +0100

    add perlin noise

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use kiss2d::clrs::*;
 use kiss2d::{Canvas, Key, minifb};
+use noise::{Perlin, NoiseFn};
 
 const WIDTH: usize = 1280;
 const HEIGHT: usize = 720;
@@ -7,11 +8,17 @@ const HEIGHT: usize = 720;
 static TITLE: &str = "Noise Test - Press ESC to exit";
 
 fn main() -> minifb::Result<()> {
+    let perlin = Perlin::new();
     let mut canvas = Canvas::new(TITLE, WIDTH, HEIGHT)?;
     while canvas.is_open() && !canvas.is_keydown(Key::Escape) {
         for x in (0..WIDTH).step_by(10) {
             for y in (0..HEIGHT).step_by(10) {
-                canvas.circle((x as isize, y as isize), 3, RED);
+                let n = perlin.get([x as f64 / 1000.0, y as f64 / 1000.0]);
+                if n < 0.0 {
+                    canvas.circle((x as isize, y as isize), 3, RED);
+                } else {
+                    canvas.circle((x as isize, y as isize), 3, BLUE);
+                }
             }
         }
         canvas.udpate();
